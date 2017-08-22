9ea126eabab37a649a4bde8815c7a502288920fc
Fernando Vladimir Montes Arevalo <fernando.montes@telusinternational.com>
2017-08-22 16:46:09 -0600

    Clase Conexion

--- src/com/conexion/Conexion.java
+++ src/com/conexion/Conexion.java
@@ -1,14 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 /**
  *
  * @author fernando.montes
  */
 public class Conexion {
+
+    private Connection con;
+
+    public Connection getCon() {
+        return con;
+    }
+
+    public void setCon(Connection con) {
+        this.con = con;
+    }
     
+    public void conectar() throws Exception{
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_viajes?user=tare&password=toor");    //Cambiar por el respectivo puerto, usuario y password
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+    
+    public void desconectar() throws Exception{
+        try {
+            if(con != null){
+                if(con.isClosed() == false){
+                    con.close();
+                }
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+    }
 }
