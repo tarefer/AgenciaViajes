41e2422d653df89a63917d97582a087b329891f9
Fernando Vladimir Montes Arevalo <fernando.montes@telusinternational.com>
2017-08-22 19:52:56 -0600

    Agregando modelo de Cliente

---
+++ src/com/modelo/Cliente.java
@@ -0,0 +1,118 @@
+
+package com.modelo;
+
+/**
+ * Nombre de la clase: Cliente
+ * Fecha: 22/08/2017
+ * Versión: 1.0
+ * Copyright: Fernando Montes
+ * @author fernando.montes
+ */
+public class Cliente {
+    private int id_cliente;
+    private String nombres;
+    private String apellidos;
+    private String genero;
+    private int telefono;
+    private String correo;
+    private String nacionalidad;
+    private String tipo_documento;
+    private int num_documento;
+    private String direccion;
+    private int id_departamento;
+
+    public Cliente() {
+    }
+
+    public int getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(int id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getTipo_documento() {
+        return tipo_documento;
+    }
+
+    public void setTipo_documento(String tipo_documento) {
+        this.tipo_documento = tipo_documento;
+    }
+
+    public int getNum_documento() {
+        return num_documento;
+    }
+
+    public void setNum_documento(int num_documento) {
+        this.num_documento = num_documento;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getId_departamento() {
+        return id_departamento;
+    }
+
+    public void setId_departamento(int id_departamento) {
+        this.id_departamento = id_departamento;
+    }
+    
+    
+    
+    
+}
