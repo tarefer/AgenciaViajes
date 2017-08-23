f285ea81c3f468df2878db3c11161b8a9e463b18
Fernando Vladimir Montes Arevalo <fernando.montes@telusinternational.com>
2017-08-22 20:10:48 -0600

    Agregando el modelo de Pais

---
+++ src/com/modelo/Pais.java
@@ -0,0 +1,63 @@
+
+package com.modelo;
+
+/**
+ * Nombre de la clase: Pais
+ * Fecha: 22/08/2017
+ * Versión: 1.0
+ * Copyright: Fernando Montes   
+ * @author fernando.montes
+ */
+public class Pais {
+    private int id_pais;
+    private String nombre;
+    private String aeropuerto;
+    private String area;
+    private String restricciones;
+
+    public Pais() {
+    }
+
+    public int getId_pais() {
+        return id_pais;
+    }
+
+    public void setId_pais(int id_pais) {
+        this.id_pais = id_pais;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAeropuerto() {
+        return aeropuerto;
+    }
+
+    public void setAeropuerto(String aeropuerto) {
+        this.aeropuerto = aeropuerto;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getRestricciones() {
+        return restricciones;
+    }
+
+    public void setRestricciones(String restricciones) {
+        this.restricciones = restricciones;
+    }
+    
+    
+
+}
